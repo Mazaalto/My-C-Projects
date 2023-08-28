a2f68dd8064c0a11bd0235257730c1e71fd2af04
Matias Aalto-Setälä <42780325+Mazaalto@users.noreply.github.com>
2023-08-28 09:02:46 +0300

    Update main.cpp
    Updated inheritance to make playable character, also updated an attack method that uses pointers

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 #include <string>
 
 using namespace std;
+// std:: optional when using namespace std,
+// eg. cout << "Hello, World!" << endl; is valid
 
 class CharacterOfTheGame {
     public:
         std::string name;
+        static std::string type;
         int maxHealth;
         int currentHealth;
         int damage;
@@ -16,7 +19,23 @@ class CharacterOfTheGame {
         // functions, methods
         void takeDamage(int);
         void healCharacter(int);
+        static void changeTypeOfCharacter(std::string);
 };
+// inheritance
+class PlayerCharacter: public CharacterOfTheGame {
+    public:
+        int level;
+        PlayerCharacter(std::string, int, int, int, int);
+
+};
+
+PlayerCharacter::PlayerCharacter(std::string _name, int _maxHealth, int _currentHealth, int _damage, int _level)
+: CharacterOfTheGame(_name, _maxHealth, _currentHealth, _damage) {
+    level = _level;
+}
+// type is same for every instance, if changed changes same in every instance
+std::string CharacterOfTheGame::type = "Non player character";
+
 
 CharacterOfTheGame::CharacterOfTheGame(std::string _name, int _maxHealth, int _currentHealth, int _damage) {
     name = _name;
@@ -25,6 +44,7 @@ CharacterOfTheGame::CharacterOfTheGame(std::string _name, int _maxHealth, int _c
     damage = _damage;
     isAlive = true;
 }
+
 
 void CharacterOfTheGame::takeDamage(int amount) {
     currentHealth -= amount;
@@ -39,15 +59,24 @@ void CharacterOfTheGame::healCharacter(int amount) {
         currentHealth = maxHealth;
     }
 }
+// static functions cannot change any values instance related
+void CharacterOfTheGame::changeTypeOfCharacter(std::string newType) {
+    type = newType;
+    // this gives error: currentHealth = 0;
+
+}
+// Character 1 attacks Character 2, with the power of that character, by using pointers
+void attackCharacter (CharacterOfTheGame *characterPointer1, CharacterOfTheGame *characterPointer2) {
+    int damageTaken = characterPointer1 -> damage;
+    characterPointer2-> takeDamage(damageTaken);
+}
+
+
 
 int main() {
     CharacterOfTheGame character = CharacterOfTheGame("Matias", 100, 100, 10);
-    character.takeDamage(10);
-
-    std::cout << character.currentHealth << std::endl;
-    std::cout << character.isAlive << std::endl;
-
-    character.healCharacter(10);
+    CharacterOfTheGame character2 = CharacterOfTheGame("Enzio", 100, 100, 20);
+    attackCharacter(&character2, &character);
 
     std::cout << character.currentHealth << std::endl;
     std::cout << character.isAlive << std::endl;
